02e716b3afe32f4523534bc1b01e7f5619cd4bdd
MIPAN <kadekisaka00@gmail.com>
2026-04-07 19:06:50 +0800

    Create Types.types.ts

---
+++ frontend/src/types/Types.types.ts
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react"
+
+export interface ButtonInterface  {
+    children : string,
+    onClick: () => void
+}
+
+export interface props {
+    children: ReactNode
+}
