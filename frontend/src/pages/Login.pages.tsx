be6e29cf05364c7b36106edeb8dc0279153b4ac4
MIPAN <kadekisaka00@gmail.com>
2026-04-07 18:57:08 +0800

    Update Login.pages.tsx

--- frontend/src/pages/Login.pages.tsx
+++ frontend/src/pages/Login.pages.tsx
@@ -1,1 +1,11 @@
+import GoogleLoginComponents from "../components/Google_login.components";
 
+const Login = () => {
+  return (
+    <div className="justify-center items-center min-h-screen flex">
+      <GoogleLoginComponents />
+    </div>
+  );
+};
+
+export default Login;
