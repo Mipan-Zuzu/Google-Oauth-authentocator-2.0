ec3139815f2a4b129c7a4552eb7da4406532187b
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-04-10 15:33:18 +0800

    fix

--- frontend/src/security/Midlewere.security.tsx
+++ frontend/src/security/Midlewere.security.tsx
@@ -9,7 +9,7 @@ const MidlewereSecurity = ({children}: props) => {
     useEffect(() => {
         const checkingData =  async() => {
         try {
-        const test = await axios.get("http://localhost:3000/auth/testing", {
+        const test = await axios.get("https://oauth-apis.koyeb.app/auth/testing", {
             withCredentials: true
         })
         log(test)
