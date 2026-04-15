5babd441fe4a4da6c43a07bba5c4b25c4fe3899b
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-04-16 01:30:20 +0800

    feat : add redis session and cookie sid client

--- backend/src/service/auth.service.ts
+++ backend/src/service/auth.service.ts
@@ -11,6 +11,7 @@ import { client } from "./auth/google.js"
 import {userOauth} from "../model/databse.model.js"
 import { redis } from "../service/redis/redis.js"
 import { handleError } from "../utils/asyncHandler.js"
+import { devNull } from "os"
 
 //* config
 dotenv.config()
@@ -42,19 +43,6 @@ export const auth_google_callback = async (req: Request, res: Response): Promise
         handleError(res, error_message, 401)
         return
     }
-    
-    const payload: {token: string} = {
-        token: token_code
-    }
-    const token = jwt.sign(payload, KEY_TOKEN_JWT, {
-        expiresIn: "5m"
-    })
-
-    if(!token || typeof token !== "string" || token.trim() === "") {
-        const error_message = "token code is undefined type string"
-        handleError(res, error_message, 404)
-        return
-    }
 
     const {tokens} = await client.getToken(token_code as string)
 
@@ -64,17 +52,35 @@ export const auth_google_callback = async (req: Request, res: Response): Promise
         return
     }
 
+
+// Todod : using try Catch errror
+try {
+
+    // Todo: verify client login
     const ticket = await client.verifyIdToken({
         idToken: tokens.id_token,
         audience: ID_CLIENT
     })
 
-        const ticket_payload = await ticket.getPayload()
-        const googleId = ticket_payload?.sub ? ticket_payload.sub : "failed to get data"
-        const find_sub = await userOauth.findOne({googleId: googleId})
+    //  Todo: get paylaod client login
+    const ticket_payload = await ticket.getPayload()
+
+    const googleId = ticket_payload?.sub ? ticket_payload.sub : "failed to get data"
+    //  Todo: find client login. data profile
+    const find_sub = await userOauth.findOne({googleId: googleId})
         const parses_token = typeof tokens === 'string' ? JSON.parse(tokens) : tokens
 
-        if(find_sub == null || !find_sub) {
+
+        if(find_sub || find_sub !== null) {
+            res.status(409).json(
+                {
+                    data : "user registration data has been added previously",
+                    status : 409
+                }
+            )
+        }
+
+        //  Todo: set scema client login. data profile
         const user_login = new userOauth({
            googleId : ticket_payload?.sub,
            name : ticket_payload?.name, 
@@ -83,25 +89,64 @@ export const auth_google_callback = async (req: Request, res: Response): Promise
            avatar : ticket_payload?.picture,
            role : role_default
         })
+        await user_login.save()
         console.log({
             parses_token : parses_token,
             user_login : user_login,
             info: "kumpulan data parses_token dan user login"
         })
+
+    console.log({
+        find_sub : !find_sub?._id? user_login._id.toString() : find_sub._id.toString(),
+        info : "data di db client id",
+        binding : "69dfca20d4e4cdcc7da59b74",
+        status : 200
+    })
+
+    
+    const sid = crypto.randomUUID()
+    
+    const payload = {
+        sessionId : `sid_${sid}`,
+        userId :  !find_sub?._id? user_login._id.toString() : find_sub._id.toString(),
+        googleSub : ticket_payload?.sub,
+        email : ticket_payload?.email
     }
 
+    await redis.set(`session:sid_${sid}`, payload, {ex: 3600})
 
-    res.cookie("token", tokens, {
+    res.cookie("sid", `sid_${sid}`, {
         httpOnly: true,
         sameSite: "lax",
         maxAge: 60 * 60 * 1000,
     })
+
+    res.status(200).json({
+        data : "valid",
+        status : 200
+    })
+
+} catch (error) {
+    if(error instanceof Error) {
+        res.status(500).json({
+            data : error.message,
+            status : 500
+        })
+        return
+    }
+}
     
     res.redirect(URL_FRONTEND!)
 }
 
 export const checking = async (req: Request, res: Response): Promise<void> => {
-    
+    // Todod: jadi simpan hasil 
+    const payload = {
+    "sessionId": "sess_abc123xyz",
+    "userId": "123",
+    "googleSub": "10987654321",
+    "email": "user@gmail.com"
+    }
 }
 
 export const checking_login_user = async (req: Request, res: Response): Promise<void> => {
@@ -111,7 +156,6 @@ export const checking_login_user = async (req: Request, res: Response): Promise<
         handleError(res, error_message, 404)
         return
     }
-
     //* verify token
     // const verify = jwt.verify(token_access, KEY_TOKEN_JWT)
     const find_user_login = await userOauth.findOne() //TODO: FIND USER LOGIN MONGO
